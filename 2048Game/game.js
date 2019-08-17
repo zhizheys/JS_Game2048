7c633290f3235122fee391aac3617678f0ea168d
zhizheys <zhizheys@126.com>
2019-08-17 12:51:46 +0800

    add moveRight,moveDown

--- 2048Game/game.js
+++ 2048Game/game.js
@@ -10,6 +10,7 @@
             var len =opt.len
             var size = opt.size
             var margin= opt.margin;
+            
 
             var view = new View(prefix,len,size,margin);
             view.init();
@@ -26,7 +27,19 @@
             board.generate()
 
             board.onMove=function(e){
+
                 view.move(e.from,e.to);
+
+                if(e.to.num > e.from.num){
+                    this.score = this.score + e.to.num;
+                    view.updateScore(this.score);
+
+                    if(this.score >= this.winScore){
+                        this.isGameOver=true;
+                        view.winGame();
+                    }
+                }
+
             }
 
             board.onMoveComplete = function(e){
@@ -39,14 +52,25 @@
             }
 
             $(document).keydown(
-                
                 function(e){
+
+                    if(board.isGameOver){
+                        return false;
+                    }
+
                     switch(e.which){
                         case 37:
-                            console.log('to left');
                             board.moveLeft();
                             break;
-                        
+
+                        case 38:
+                            board.moveUp();
+                            break;
+
+                        case 39:
+                            board.moveRight();
+                            break;
+
                         case 40:
                             console.log('to down');
                             board.moveDown();
@@ -74,7 +98,7 @@
             this.container = $("#game_container");
             var containerSize = len * size + margin * (len+1);
             this.container.css({width:containerSize,height:containerSize})
-            this.nums ={}
+            this.nums ={};
         }
 
         View.prototype ={
@@ -137,12 +161,22 @@
                     }
                 })
 
+            },
+            updateScore:function(score){
+                var $score = $('#' + this.prefix + '_score');
+                $score.text(score);
+            },
+            winGame:function(){
+                alert('win game');
             }
         }
 
         function Board(len){
             this.len =len;
             this.arr =[];
+            this.score=0;
+            this.winScore=50;
+            this.isGameOver=false;
 
         }
 
@@ -183,70 +217,129 @@
                 console.log("on generate")
             },
             moveLeft:function(){
+                console.log('left')
                 var moved = false;
 
                 for(var x=0,len = this.arr.length;x<len;++x){
-                    for(var y=0,arr=this.arr[x];y<len;++y){
 
-                        for(var next =y+1;next <len;++next){
-                            if(arr[next] ==0){
-                                continue;
+                    var arr=this.arr[x]
+
+                    for(var y=1;y<len;y++){
+                        if(arr[y]==0){
+                            continue;
+                        }else{
+
+                            for(var j=y;j>0;j--){
+
+                                if(arr[j-1]==0){
+                                    arr[j-1] =arr[j];
+                                    var next=j-1;
+                                    this.onMove({from:{x:x,y:j,num:arr[j]},
+                                        to:{x:x,y:next,num:arr[j-1]}});
+    
+                                    arr[j]=0;
+                                    moved=true;
+    
+                                }else{
+                                    if(arr[j-1] == arr[j]){
+                                        arr[j-1] *=2;
+                                        var next=j-1;
+    
+                                        this.onMove({from:{x:x,y:j,num:arr[j]},
+                                            to:{x:x,y:next,num:arr[j-1]}});
+    
+                                        arr[j]=0;
+                                        moved=true;
+                                    }
+                                }
+
                             }
 
-                            if(arr[y]===0){
-                                arr[y] =arr[next];
-                                this.onMove({from:{x:x,y:next,num:arr[next]},
-                                            to:{x:x,y:y,num:arr[y]}});
-
-                                arr[next] =0;
-                                moved=true;
-
-                            } else if(arr[y]===arr[next]){
-                                arr[y] *=2;
-                                this.onMove({from:{x:x,y:next,num:arr[next]},
-                                    to:{x:x,y:y,num:arr[y]}});
-
-                                arr[next] =0;
-                                moved=true;
-                            }
-
-                            break;
                         }
+                    
+                    }
+                }
+
+                this.onMoveComplete({moved:moved})
+            },
+            moveRight:function(){
+                console.log('right')
+                var moved = false;
+
+                for(var x=0,len = this.arr.length;x<len;++x){
+                    var arr=this.arr[x]
+                    for(var y=0;y<len-1;y++){
+
+                        if(arr[y]==0){
+                            continue;
+                        }else{
+
+                            if(arr[y+1]==0){
+                                arr[y+1] =arr[y];
+                                var next=y+1;
+                                this.onMove({from:{x:x,y:y,num:arr[y]},
+                                    to:{x:x,y:next,num:arr[y+1]}});
+
+                                arr[y]=0;
+                                moved=true;
+
+                            }else{
+                                if(arr[y+1] == arr[y]){
+                                    arr[y+1] *=2;
+                                    var next=y+1;
+
+                                    this.onMove({from:{x:x,y:y,num:arr[y]},
+                                        to:{x:x,y:next,num:arr[y+1]}});
+
+                                    arr[y]=0;
+                                    moved=true;
+                                }
+                            }
+
+                        }
+                    
                     }
                 }
 
                 this.onMoveComplete({moved:moved})
             },
             moveDown:function(){
+                console.log('down')
                 var moved = false;
+                var arr=this.arr;
 
-                for(var x=0,len = this.arr.length;x<len;++x){
-                    for(var y=0,arr=this.arr[x];y<len;++y){
+                for(var x=0,len = this.arr.length;x<len-1;++x){
+               
+                    for(var y=0;y<len;y++){
 
-                        for(var next =y+1;next <len;++next){
-                            if(arr[next] ==0){
-                                continue;
-                            }
+                        if(arr[x][y]==0){
+                            continue;
+                        }else{
 
-                            if(arr[y]===0){
-                                arr[y] =arr[next];
-                                this.onMove({from:{x:x,y:next,num:arr[next]},
-                                            to:{x:x,y:y,num:arr[y]}});
+                            if(arr[x+1][y]==0){
+                                arr[x+1][y] =arr[x][y];
+                                var next=x+1;
+                                this.onMove({from:{x:x,y:y,num:arr[x][y]},
+                                    to:{x:next,y:y,num:arr[x+1][y]}});
 
-                                arr[next] =0;
+                                arr[x][y]=0;
                                 moved=true;
 
-                            } else if(arr[y]===arr[next]){
-                                arr[y] *=2;
-                                this.onMove({from:{x:x,y:next,num:arr[next]},
-                                    to:{x:x,y:y,num:arr[y]}});
+                            }else{
+                                if(arr[x+1][y] == arr[x][y]){
+                                    arr[x+1][y] *=2;
+                                    var next=x+1;
 
-                                arr[next] =0;
-                                moved=true;
+                                    this.onMove({from:{x:x,y:y,num:arr[x][y]},
+                                        to:{x:next,y:y,num:arr[x+1][y]}});
+
+                                    arr[x][y]=0;
+                                    moved=true;
+                                }
                             }
 
-                            break;
                         }
+                    
                     }
                 }
 
